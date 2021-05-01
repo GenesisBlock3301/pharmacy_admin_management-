bf55996b53cd71731943f0f14fa68582390cac74
Nur Amin Sifat <nur15-1463@diu.edu.bd>
2021-05-01 22:15:58 +0600

    database

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -3,7 +3,27 @@ from .models import *
 
 
 class SalesManagementAdmin(admin.ModelAdmin):
-    list_display = ('medicine_name', 'number_of_medicine','original_price','selling_price','created_at')
+    list_display = (
+        'medicine_name', 'number_of_medicine', 'sold_number_of_medicine', 'original_price', 'selling_price',
+        'created_at')
+    readonly_fields = ['sold_number_of_medicine']
 
 
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['customer_name', 'address', 'phone_number', 'medicine_price', 'payment', 'created_at']
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'employee_name',
+        'address',
+        'phone_number',
+        'salary_amount',
+        'payment',
+        'created_at'
+    )
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Customer, CustomerAdmin)
 admin.site.register(SalesManagement, SalesManagementAdmin)
--- mainApp/models.py
+++ mainApp/models.py
@@ -1,32 +1,69 @@
 from django.db import models
 
 
-# Create your models here.
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    medicine_price = models.FloatField()
+    payment = models.FloatField()
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer_name
+
+    def customer_due(self):
+        if self.medicine_price > self.payment:
+            return self.medicine_price - self.payment
+
+
+class Employee(models.Model):
+    employee_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=16)
+    salary_amount = models.FloatField()
+    payment = models.FloatField()
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.employee_name
+
+    def salary_due(self):
+        if self.salary_amount > self.payment:
+            return self.salary_amount - self.payment
+
+
 class SalesManagement(models.Model):
     medicine_name = models.CharField(max_length=255, unique=True)
+    location = models.CharField(max_length=50)
     number_of_medicine = models.IntegerField(default=0)
     original_price = models.FloatField()
     selling_price = models.FloatField()
+    sold_number_of_medicine = models.IntegerField(default=0)
+    sold_at_a_time = models.IntegerField(default=0)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.medicine_name
 
+    def sold_medicine_value(self):
+        return self.sold_number_of_medicine * self.selling_price
+
     def expense(self):
         return self.original_price * self.number_of_medicine
 
     def profit(self):
-        sell_medicine_amount = self.selling_price * self.number_of_medicine
-        profit = sell_medicine_amount - self.expense()
-        if profit > 0:
-            return profit
+        if self.sold_medicine_value() > self.expense():
+            return self.sold_medicine_value() - self.expense()
 
     def loss(self):
-        sell_medicine_amount = self.selling_price * self.number_of_medicine
-        loss = sell_medicine_amount - self.expense()
-        if loss < 0:
-            return loss
+        if self.sold_medicine_value() < self.expense():
+            return self.expense() - self.sold_medicine_value()
 
-    def selling(self):
-        return self.selling_price * self.number_of_medicine
+    def save(self, *args, **kwargs):
+        if self.sold_at_a_time <= self.number_of_medicine:
+            self.number_of_medicine -= self.sold_at_a_time
+            self.sold_number_of_medicine += self.sold_at_a_time
+            self.sold_at_a_time = 0
+            return super().save(*args, **kwargs)
