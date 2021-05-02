9600466043db9a3fbe4e58078dfe60aaa2f08630
Nur Amin Sifat <nur15-1463@diu.edu.bd>
2021-05-02 13:20:57 +0600

    url

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
