ed846d7971a21381ee2eff4023211b5e4f994649
Nur Amin Sifat <nur15-1463@diu.edu.bd>
2021-06-03 15:31:11 +0600

    expire date

--- mainApp/templatetags/id_filter.py
+++ mainApp/templatetags/id_filter.py
@@ -1,5 +1,6 @@
 from django import template
 from mainApp.models.customer import Customer
+import datetime
 
 register = template.Library()
 
@@ -9,4 +10,11 @@ def id_filter(value):
     return customer.id
 
 
+def expire_date(value):
+    my_date = datetime.date.today()
+    if my_date == value:
+        return True
+
+
 register.filter('id_filter', id_filter)
+register.filter('expire_date', expire_date)
